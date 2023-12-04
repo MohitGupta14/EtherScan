37014cd5eb26f47a44a96483ade26f0318d4f4d8
Mohit Gupta <mohitgupta@Mohits-MacBook-Air-3.local>
2023-12-04 22:54:48 +0530

    Adding header comonent and updating backend

--- backend/index.js
+++ backend/index.js
@@ -2,136 +2,42 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3001;
-
-// Middleware to parse JSON
-app.use(express.json());
+const cors = require('cors');
+app.use(cors()); // Enable CORS for all routes
 const YOUR_ETHERSCAN_API_KEY = 'G3UWE3PSHIHTRVKSX6SYXEKXG4TIPFGPZ8';
 // Route to get information about a specific block
-app.get('/balance/:address', async (req, res) => {
-    try {
-      const address = req.params.address;
-      const apiKey = YOUR_ETHERSCAN_API_KEY; // Replace with your Etherscan API key
-  
-      const apiUrl = `https://api.etherscan.io/api?module=account&action=balance&address=${address}&apikey=${apiKey}`;
-      const response = await axios.get(apiUrl);
-  
-      // Check if the API request was successful
-      if (response.data.status === '1') {
-        const balance = response.data.result;
-        res.json({ balance });
-      } else {
-        res.status(500).json({ error: 'Error fetching balance' });
-      }
-    } catch (error) {
-      console.error('Error:', error.message);
-      res.status(500).json({ error: 'Internal server error' });
-    }
+
+app.get('/ethereum-price', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.etherscan.io/api', {
+      params: {
+        module: 'stats',
+        action: 'ethprice',
+        apikey: YOUR_ETHERSCAN_API_KEY,
+      },
+    });
+
+    const ethereumPrice = response.data.result.ethusd;
+    res.json({ price: ethereumPrice });
+  } catch (error) {
+    console.error('Error fetching Ethereum price:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-app.get('/block/:blockNumber', async (req, res) => {
+app.get('/ethGasPrice', async (req, res) => {
   try {
-    const blockNumber = req.params.blockNumber;
-    const apiKey = YOUR_ETHERSCAN_API_KEY;
-
-    const apiUrl = `https://api.etherscan.io/api?module=block&action=getblockreward&blockno=${blockNumber}&apikey=${apiKey}`;
+    const apiKey = YOUR_ETHERSCAN_API_KEY; // Replace with your EtherScan API key
+    const apiUrl = `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`;
     const response = await axios.get(apiUrl);
+    const gasPrice = response.data.result.ProposeGasPrice;
 
-    if (response.data.status === '1') {
-      const blockInfo = response.data.result;
-      res.json({ blockInfo });
-    } else {
-      res.status(500).json({ error: 'Error fetching block information' });
-    }
+    res.json({ gasPrice });
   } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error('Error fetching gas price:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-// Route to get information about a specific transaction
-app.get('/transaction/:transactionHash', async (req, res) => {
-  try {
-    const transactionHash = req.params.transactionHash;
-    const apiKey = YOUR_ETHERSCAN_API_KEY;
-
-    const apiUrl = `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${transactionHash}&apikey=${apiKey}`;
-    const response = await axios.get(apiUrl);
-
-    if (response.data.status === '1') {
-      const transactionInfo = response.data.result;
-      res.json({ transactionInfo });
-    } else {
-      res.status(500).json({ error: 'Error fetching transaction information' });
-    }
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Route to get information about a specific address
-app.get('/address/:address', async (req, res) => {
-  try {
-    const address = req.params.address;
-    const apiKey = YOUR_ETHERSCAN_API_KEY;
-
-    const apiUrl = `https://api.etherscan.io/api?module=account&action=balance&address=${address}&apikey=${apiKey}`;
-    const response = await axios.get(apiUrl);
-
-    if (response.data.status === '1') {
-      const balance = response.data.result;
-      res.json({ balance });
-    } else {
-      res.status(500).json({ error: 'Error fetching address information' });
-    }
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Route to get information about a specific token
-app.get('/token/:contractAddress', async (req, res) => {
-  try {
-    const contractAddress = req.params.contractAddress;
-    const apiKey = YOUR_ETHERSCAN_API_KEY;
-
-    const apiUrl = `https://api.etherscan.io/api?module=token&action=tokeninfo&contractaddress=${contractAddress}&apikey=${apiKey}`;
-    const response = await axios.get(apiUrl);
-
-    if (response.data.status === '1') {
-      const tokenInfo = response.data.result;
-      res.json({ tokenInfo });
-    } else {
-      res.status(500).json({ error: 'Error fetching token information' });
-    }
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Route to verify a smart contract
-app.post('/verify-contract', async (req, res) => {
-  try {
-    // Implementation for contract verification goes here
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Route to get blockchain statistics
-app.get('/stats', async (req, res) => {
-  try {
-    // Implementation for blockchain statistics goes here
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-// Additional routes and features can be added based on your requirements
 
 // Start the Express server
 app.listen(port, () => {
