b63255170d278b9c8957103f5ea5acdb337cb9e3
Mohit Gupta <mohitgupta@Mohits-MacBook-Air-3.local>
2023-12-05 23:22:34 +0530

    Connecting more API

--- backend/index.js
+++ backend/index.js
@@ -57,7 +57,79 @@ app.get('/address', async (req, res) => {
       startblock: 0,
       endblock: 99999999,
       page: 1,
-      offset: 25,
+      offset: 20,
+      sort: 'desc',
+      apikey: apiKey,
+    };
+
+    const response = await axios.get(apiUrl, { params });
+    res.json(response.data);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get("/totalTransactionsCount", async (req, res) => {
+  try {
+    const response = await axios.get("https://api.etherscan.io/api", {
+      params: {
+        module: "stats",
+        action: "ethsupply",
+        apikey: YOUR_ETHERSCAN_API_KEY,
+      },
+    });
+
+    const totalTransactionsCount = response.data.result;
+
+    
+    return res.status(200).json({ totalTransactionsCount });
+  } catch (error) {
+    console.error(`Error fetching total transactions count: ${error}`);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/balance", async(req,res) =>{
+  try{
+
+    const { address } = req.query;
+    const response = await axios.get("https://api.etherscan.io/api", {
+      params: {
+        module: "account",
+        action: "balance",
+        address: address,
+        tag : "latest",
+        apikey: YOUR_ETHERSCAN_API_KEY,
+      },
+    });
+    const getBalance =  response.data.result;
+    return res.status(200).json({ getBalance });
+  } catch (error) {
+    console.error(`Error fetching latest balance : ${error}`);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+})
+
+app.get('/erc20', async (req, res) => {
+  try {
+    const { address } = req.query;
+
+    if (!address) {
+      return res.status(400).json({ error: 'Address parameter is required' });
+    }
+
+    const apiUrl = 'https://api.etherscan.io/api';
+    const apiKey = 'G3UWE3PSHIHTRVKSX6SYXEKXG4TIPFGPZ8';
+
+    const params = {
+      module: 'account',
+      action: 'tokentx',
+      address: address,
+      startblock: 0,
+      endblock: 99999999,
+      page: 1,
+      offset: 20,
       sort: 'desc',
       apikey: apiKey,
     };
@@ -71,8 +143,94 @@ app.get('/address', async (req, res) => {
 });
 
 
+app.get('/latestBlocks', async (req, res) => {
+  try {
+    // Get the latest block number
+    const apiUrl = 'https://api.etherscan.io/api';
+    const apiKey = 'G3UWE3PSHIHTRVKSX6SYXEKXG4TIPFGPZ8';
 
-// Start the Express server
+    const latestBlockResponse = await axios.get(apiUrl, {
+      params: {
+        module: 'proxy',
+        action: 'eth_blockNumber',
+        apiKey: apiKey,
+      },
+    });
+
+    const latestBlockNumber = parseInt(latestBlockResponse.data.result, 16);
+
+    // Fetch block details for the last 6 blocks
+    const blockDetails = [];
+    for (let i = latestBlockNumber; i > latestBlockNumber - 6; i--) {
+      const blockResponse = await axios.get(apiUrl, {
+        params: {
+          module: 'proxy',
+          action: 'eth_getBlockByNumber',
+          tag: `0x${i.toString(16)}`,
+          apiKey: apiKey,
+          boolean:true
+        },
+      });
+
+      const blockData = blockResponse.data.result;
+      const timestamp = parseInt(blockData.timestamp, 16) * 1000; // Convert to milliseconds
+      const timeAgo = Math.floor((Date.now() - timestamp) / 1000); // Time in seconds
+
+      blockDetails.push({
+        blockHeight: i,
+        timestamp: timestamp,
+        timeAgo: timeAgo,
+        txnHash: blockData.transactions[0], // You can choose which transaction hash to include
+      });
+    }
+
+    res.json({ latestBlocks: blockDetails });
+  } catch (error) {
+    console.error('Error fetching latest block details:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/latestTransactions', async (req, res) => {
+  try {
+    // Fetch recent transactions using the eth_getLogs endpoint
+    const apiUrl = 'https://api.etherscan.io/api';
+    const apiKey = 'G3UWE3PSHIHTRVKSX6SYXEKXG4TIPFGPZ8';
+    const logsResponse = await axios.get(apiUrl, {
+      params: {
+        module: 'logs',
+        action: 'getLogs',
+        fromBlock: 'latest',
+        toBlock: 'latest',
+        sort: 'desc',
+        apiKey: apiKey,
+      },
+    });
+
+    const transactions = [];
+    const logEntries = logsResponse.data.result || [];
+    // Process each log entry
+    logEntries.forEach((log) => {
+      const timestamp = parseInt(log.timeStamp);
+      const timeAgo = Math.floor((Date.now() / 1000 - timestamp) / 60); // Time in minutes
+
+      transactions.push({
+        timestamp: timestamp,
+        timeAgo: timeAgo,
+        txnHash: log.transactionHash,
+      });
+    });
+
+    res.json({ latestTransactions: transactions });
+  } catch (error) {
+    console.error('Error fetching latest transactions:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
