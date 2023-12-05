b351a6f05b57ab6341831f58823e216858e1335b
Mohit Gupta <mohitgupta@Mohits-MacBook-Air-3.local>
2023-12-05 12:02:06 +0530

    Search section 01

--- backend/index.js
+++ backend/index.js
@@ -39,6 +39,39 @@ app.get('/ethGasPrice', async (req, res) => {
   }
 });
 
+app.get('/address', async (req, res) => {
+  try {
+    const { address } = req.query;
+
+    if (!address) {
+      return res.status(400).json({ error: 'Address parameter is required' });
+    }
+
+    const apiUrl = 'https://api.etherscan.io/api';
+    const apiKey = 'G3UWE3PSHIHTRVKSX6SYXEKXG4TIPFGPZ8';
+
+    const params = {
+      module: 'account',
+      action: 'txlist',
+      address: address,
+      startblock: 0,
+      endblock: 99999999,
+      page: 1,
+      offset: 25,
+      sort: 'desc',
+      apikey: apiKey,
+    };
+
+    const response = await axios.get(apiUrl, { params });
+    res.json(response.data);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+
 // Start the Express server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
